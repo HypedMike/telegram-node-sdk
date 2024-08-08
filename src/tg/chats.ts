f1361b73112b2860b1c0c8e4d6f5b1936e9b64fd
Mike <michele.saladino@outlook.it>
2024-08-08 20:14:45 +0200

    general improvments

--- src/tg/chats.ts
+++ src/tg/chats.ts
@@ -19,12 +19,55 @@ export interface TelegramChat {
         max_reaction_count: number;
         accent_color_id: number;
     }>;
+
+    /**
+     * 
+     * @param chatId 
+     * @returns invite link
+     * 
+     * rapresenting the chat invite link
+     */
     chatInviteLink: (chatId: number) => Promise<string>;
     chatAdministratorRights: (chatId: number) => Promise<string>;
     chatMember: (chatId: number) => Promise<string>;
     chatMemberOwner: (chatId: number) => Promise<string>;
     chatJoinRequest: (chatId: number) => Promise<string>;
     chatPermissions: (chatId: number) => Promise<string>;
+
+    // chat settings
+
+    /**
+     * 
+     * @param chatId 
+     * @param photo 
+     * @returns `true` on success
+     * 
+     * Use this method to set a new profile photo for the chat. 
+     * Photos can't be changed for private chats. 
+     * The bot must be an administrator in the chat for this to work and 
+     * must have the appropriate administrator rights.
+     */
+    setChatPhoto: (chatId: number, photo: File) => Promise<boolean>;
+
+    /**
+     * 
+     * @param chatId 
+     * @param title 
+     * @returns boolean
+     * 
+     * Use this method to change the title of a chat. 
+     * Titles can't be changed for private chats. 
+     * The bot must be an administrator in the chat 
+     * for this to work and must have the appropriate administrator rights.
+     */
+    setChatTitle: (chatId: number, title: string) => Promise<boolean>;
+
+    /**
+     * Use this method to delete a chat photo. Photos can't be changed for private chats. 
+     * The bot must be an administrator in the chat for this to work and 
+     * must have the appropriate administrator rights.
+     */
+    deleteChatPhoto: (chatId: number) => Promise<boolean>;
 }
 
 const chatPhoto: (chatId: number) => Promise<{
@@ -46,13 +89,19 @@ const chatPhoto: (chatId: number) => Promise<{
 }
 
 const chatInviteLink = async (chatId: number) => {
-    console.log('Getting chat invite link:', chatId);
-    return 'chatInviteLink';
+    const response = await tg_fetch('chatInviteLink', [
+        { key: 'chat_id', value: chatId },
+    ]);
+
+    return response as string;
 }
 
 const chatAdministratorRights = async (chatId: number) => {
-    console.log('Getting chat administrator rights:', chatId);
-    return 'chatAdministratorRights';
+    const response = await tg_fetch('chatAdministratorRights', [
+        { key: 'chat_id', value: chatId },
+    ]);
+
+    return response as string;
 }
 
 const chatMember = async (chatId: number) => {
@@ -75,6 +124,34 @@ const chatPermissions = async (chatId: number) => {
     return 'chatPermissions';
 }
 
+const setChatPhoto = async (chatId: number, photo: File) => {
+    const fd = new FormData();
+    fd.append('photo', photo);
+
+    const response = await tg_fetch('setChatPhoto', [
+        { key: 'chat_id', value: chatId },
+    ], "POST", fd)
+
+    return response as boolean;
+}
+
+const setChatTitle = async (chatId: number, title: string) => {
+    const response = await tg_fetch('setChatTitle', [
+        { key: 'chat_id', value: chatId },
+        { key: 'title', value: title },
+    ]);
+
+    return response as boolean;
+}
+
+const deleteChatPhoto = async (chatId: number) => {
+    const response = await tg_fetch('deleteChatPhoto', [
+        { key: 'chat_id', value: chatId },
+    ]);
+
+    return response as boolean;
+}
+
 export const telegramChat: TelegramChat = {
     chatPhoto,
     chatInviteLink,
@@ -83,4 +160,7 @@ export const telegramChat: TelegramChat = {
     chatMemberOwner,
     chatJoinRequest,
     chatPermissions,
+    setChatPhoto,
+    setChatTitle,
+    deleteChatPhoto
 }
