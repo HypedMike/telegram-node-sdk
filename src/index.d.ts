a08f94d583ad1547dcdb3f95dfc7a490db0e814e
Mike <michele.saladino@outlook.it>
2024-08-12 09:38:57 +0200

    added declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+import { TelegramChat } from './tg/chats';
+import ChatPhoto from './tg/models/chat_photo';
+import Chat from './tg/models/chat';
+import Update from './tg/models/update';
+import {WebhookInfo} from './tg/models/webhookinfo';
+import User from './tg/models/user';
+
+export {
+    TelegramChat,
+    ChatPhoto,
+    Chat,
+    Update,
+    WebhookInfo,
+    User
+}
